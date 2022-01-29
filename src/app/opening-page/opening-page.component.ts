17ebc5558de028bfba9173c7995bd1c938b549a5
paulinajewulska <paulina.jewulska@gmail.com>
2022-01-29 15:49:37 +0100

    Add minor fix

--- src/app/opening-page/opening-page.component.ts
+++ src/app/opening-page/opening-page.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import TITLES from '../../labels/titles.json';
+import PARAGRAPHS from '../../labels/paragraphs.json';
+import BUTTONS from '../../labels/buttons.json';
 
 @Component({
   selector: 'app-opening-page',
   templateUrl: './opening-page.component.html',
   styleUrls: ['./opening-page.component.scss']
 })
-export class OpeningPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class OpeningPageComponent {
+  title = TITLES.TimeForTastyCoffeee;
+  paragraph = PARAGRAPHS.Lorem;
+  buttonLabel = BUTTONS.letsStart;
 }
